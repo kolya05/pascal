df22929d720527817f57e48da7414649423ae6bb
kolya05 <chekotko1902@gmail.com>
2019-10-13 14:56:32 +0300

    lessond end

---
+++ 15/demo.pas
@@ -0,0 +1,13 @@
+﻿var
+  amount, count, number: integer;
+
+begin
+  read(amount);
+  count := 0;
+  for var i := 1 to amount do
+  begin
+    read(number);
+    if (number mod 4 = 0) and (number mod 7 <> 0) then count += 1;
+  end;
+  write(count);
+end.
