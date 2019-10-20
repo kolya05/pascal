836a50c5af397ff7f371486472692747ccdb6189
Qvocha <pineappletoe@gmail.com>
2019-10-20 14:22:37 +0300

    1 done

---
+++ 201019/1.pas
@@ -0,0 +1,12 @@
+﻿program shit;
+var
+  age, x: integer;
+  
+begin
+  writeln ('Введите возраст');
+  readln(age);
+  x:= age mod 10;
+  if x = 1 then write(age, ' год');
+  if (x >= 2) and (x <= 4) then write (age, ' года');
+  if (x = 0) or (x >= 5)  and (x <= 9) then write (age, ' лет');
+end.
