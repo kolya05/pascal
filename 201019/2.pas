f4b42187cb897dceda6fd161fe5b40a5829dea91
Qvocha <pineappletoe@gmail.com>
2019-10-20 14:35:53 +0300

    2 done

---
+++ 201019/2.pas
@@ -0,0 +1,11 @@
+﻿program task2;
+
+var
+  a, b, c, k:integer;
+  
+begin
+  read(a, b, c, k);
+  if a mod k = 0 then writelnFormat('число {0} - делитель числа {1}', k, a);
+  if b mod k = 0 then writelnFormat('число {0} - делитель числа {1}', k, b);
+  if c mod k = 0 then writelnFormat('число {0} - делитель числа {1}', k, c);
+end.
