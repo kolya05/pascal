68a997e4dfca5b0d4d5757cf2a559b9e4ff8e364
Qvocha <pineappletoe@gmail.com>
2019-10-23 19:42:42 +0300

    Create 20.pas

---
+++ 15/20.pas
@@ -0,0 +1,14 @@
+﻿var
+  n, x, max: integer;
+
+begin
+  read(n);
+  max := integer.MinValue;
+  for var i := 1 to n do
+  begin
+    read(x);
+    if (x > max) and (x mod 5 = 0)
+    then max := x;
+  end;
+  write(max);
+end.
