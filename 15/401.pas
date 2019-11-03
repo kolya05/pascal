7a931c00f3c4a247f30d51710a57f6d77cb0cbcf
Qvocha <pineappletoe@gmail.com>
2019-11-03 18:26:04 +0300

    Create 401.pas

---
+++ 15/401.pas
@@ -0,0 +1,23 @@
+﻿var
+  n, speed, amount, sum: integer;
+  v: real;
+
+begin
+  read(n);
+  
+  sum := 0;
+  for var i := 1 to n do
+  begin
+    read(v);
+    speed := round(v);
+    sum += speed;
+    if speed >= 60 then amount += 1;
+  end;
+  
+  writeln(sum / n);
+  if amount > 0
+  then
+    write('YES')
+  else
+    write('NO');
+end.
