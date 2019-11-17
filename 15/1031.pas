a9b0c4dc5d3016a4a280170eb965d4176e697066
Qvocha <pineappletoe@gmail.com>
2019-11-17 13:46:05 +0300

    lesson end

---
+++ 15/1031.pas
@@ -0,0 +1,14 @@
+﻿var
+  s, x: integer;
+
+begin
+  read(x);
+  s := 0;
+  while x <> 0 do
+  begin
+    if (x mod 6 = 0) and (x mod 10 = 4)
+    then s := s + x;
+    read(x);
+  end;
+  writeln(s);
+end.
---
+++ 15/260.pas
@@ -0,0 +1,14 @@
+﻿var
+  n, x, s: integer;
+
+begin
+  read(n);
+  s := 0;
+  for var i := 1 to n do
+  begin
+    read(x);
+    if x mod 10 = 6 
+    then s := s + 1;
+  end;
+  write(s);
+end.
