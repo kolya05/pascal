162eac86807eaf6c5861f8b8c3471712a529ad70
Qvocha <pineappletoe@gmail.com>
2019-12-08 14:47:40 +0300

    Create 1.pas

---
+++ 08122019/1.pas
@@ -0,0 +1,16 @@
+﻿var
+  n, max, d: integer;
+
+begin
+  read(n);
+  max := integer.MinValue;
+  while n <> 0 do
+  begin
+    d := n mod 10;
+    if d mod 2 <> 0 then write(d, ' ');
+    if d > max then max := d;
+    n := n div 10;
+  end;
+  writeln;
+  write(max);
+end.
