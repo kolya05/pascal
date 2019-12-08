ed1854b2732470e53eae6480a24fcd594edfddaf
Qvocha <pineappletoe@gmail.com>
2019-12-08 15:09:06 +0300

    lesson end

---
+++ 08122019/2.pas
@@ -0,0 +1,14 @@
+﻿var
+  s, x: integer;
+  
+begin
+  s := 0;
+  for var i := 1 to 30 do 
+  begin
+    x := random(-50, 50);
+    write(x, ' ');
+    s += x;
+  end;
+  writeln;
+  write(s / 30);
+end.
---
+++ 08122019/3.pas
@@ -0,0 +1,18 @@
+﻿var
+  max, min, a, b, c: integer;
+
+begin
+  read(a, b, c);
+  
+  max := integer.MinValue;
+  if a > max then max := a;
+  if b > max then max := b;
+  if c > max then max := c;
+  
+  min := integer.MaxValue;
+  if a < min then min := a;
+  if b < min then min := b;
+  if c < min then min := c;
+  
+  writeln(min + max);
+end.
