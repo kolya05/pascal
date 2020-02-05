675305ec062ec5875dac152b5b0b70746d88f5a2
Qvocha <pineappletoe@gmail.com>
2020-02-05 20:01:44 +0300

    Create 1a.pas

---
+++ 05022020/1a.pas
@@ -0,0 +1,12 @@
+﻿const
+  n = 10;
+
+var
+  a: array[1..n] of integer;
+  
+begin
+  for var i := 1 to n do
+    read(a[i]);
+  for var i:= 1 to n do
+    if a[i] mod 2 = 1 then write(a[i], ' ');
+end.
