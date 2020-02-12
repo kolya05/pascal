3a36c17ff9526b9558c75cacf0ffe0bf8aa129a6
Qvocha <pineappletoe@gmail.com>
2020-02-13 00:04:24 +0300

    Create 15022020.pas

---
+++ 15022020.pas
@@ -0,0 +1,18 @@
+﻿const
+  n = 5;
+  
+var 
+  a:array[1..n] of integer;
+  t: integer;
+  
+begin
+  t := 1;
+  for var i := 1 to n do
+    read(a[i]);
+  for var i := 1 to n - 1 do
+  begin
+    if a[i] > a[i + 1] then t := 0;
+  end;
+  if t = 1 then write('yes')
+  else write('no');
+end.
