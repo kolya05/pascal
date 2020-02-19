bec568c2dda58095d9512a08a40211bc2ee6125f
Qvocha <pineappletoe@gmail.com>
2020-02-19 20:19:43 +0300

    Create 19-02.pas

---
+++ 19-02.pas
@@ -0,0 +1,19 @@
+﻿const
+  n = 10;
+
+begin
+  var 
+      a:array [1..n, 1..n] of integer;
+      
+for var i := 1 to n do 
+  for var j := 1 to n do
+  begin
+    a[i, j] := j;
+  end;
+  for var i:= 1 to n do 
+    begin
+    for var j := 1 to n do
+      write(a[i, j], ' ' );
+    writeln
+    end;
+end.
